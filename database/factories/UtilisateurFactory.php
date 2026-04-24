bfcfd8151d37b45f632082affe90b8beb4ea58ba
Nansiangie <angienansi2@gmail.com>
2026-04-24 12:12:18 +0100

    mon deuxieme commit

--- database/factories/UtilisateurFactory.php
+++ database/factories/UtilisateurFactory.php
@@ -9,23 +9,19 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class UtilisateurFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-        'code_user'=> $this->faker->unique()->bothify('USR-####'),
-        'nom_user' => $this->faker->lastName(),
-        'prenom_user'=> $this->faker->firstName(),
-        'login_user'=> $this->faker->unique()->userName(),
-        'password_user'=>bcrypt('password'),
-        'tel_user'=> $this->faker->phoneNumber(),
-        'sexe_user'=> $this->faker->randomElement(['M', 'F']),
-        'role_user'=> $this->faker->randomElement(['technicien', 'client']),
-        'etat_user'=> $this->faker->randomElement(['actif', 'inactif', 'bloque'])
+            // On passe à 8 chiffres pour avoir 100 millions de combinaisons possibles
+            'code_user'     => $this->faker->unique()->numerify('USR-########'),
+            'nom_user'      => $this->faker->lastName(),
+            'prenom_user'   => $this->faker->firstName(),
+            'login_user'    => $this->faker->unique()->userName(),
+            'password_user' => bcrypt('password'), // Ou 'admin123' si tu veux un mot de passe en clair à hacher par ton modèle
+            'tel_user'      => $this->faker->phoneNumber(),
+            'sexe_user'     => $this->faker->randomElement(['M', 'F']),
+            'role_user'     => $this->faker->randomElement(['technicien', 'client']),
+            'etat_user'     => $this->faker->randomElement(['actif', 'inactif', 'bloque'])
         ];
     }
 }
